3e4fe9894233a0e97269850f207cd813a08558ab
cybertelx <cybertelx@protonmail.com>
2022-04-09 14:32:46 +0100

    Revert "eee"
    This reverts commit 16a880b1c660c75d96a33adf9221adbd5e0d870c.

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const Home = lazy(() => import('./views/Home'));
 const Farm = lazy(() => import('./views/Farm'));
 const Boardroom = lazy(() => import('./views/Boardroom'));
 const Bond = lazy(() => import('./views/Bond'));
-const Calculator = lazy(() => import('./views/Calculator'));
+const Supply = lazy(() => import('./views/Supply'));
 
 const NoMatch = () => (
   <h3 style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
@@ -57,8 +57,8 @@ const App: React.FC = () => {
             <Route path="/bond">
               <Bond />
             </Route>
-            <Route path="/calculator">
-              <Calculator />
+            <Route path="/supply">
+              <Supply />
             </Route>
             <Route path="*">
               <NoMatch />
